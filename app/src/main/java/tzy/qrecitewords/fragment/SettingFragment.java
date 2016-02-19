61963a2857868e47ac7a135da067db08ea9d1697
tuzhenyu <616871820@qq.com>
2016-02-20 01:06:17 +0800

    【增加资源】增加了主题样式资源

--- app/src/main/java/tzy/qrecitewords/fragment/SettingFragment.java
+++ app/src/main/java/tzy/qrecitewords/fragment/SettingFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.Arrays;
@@ -51,6 +52,7 @@ public class SettingFragment extends BaseFragment {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this.getActivity(), R.layout.text_layout,stringList);
 
         listView.setAdapter(arrayAdapter);
+
     }
 
     @Override
