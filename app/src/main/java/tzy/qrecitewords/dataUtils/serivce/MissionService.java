1ce77804da41aed75498473fb1de93f5dbca38db
tuzhenyu <616871820@qq.com>
2016-05-09 21:43:20 +0800

    增加每日任务

--- app/src/main/java/tzy/qrecitewords/dataUtils/serivce/MissionService.java
+++ app/src/main/java/tzy/qrecitewords/dataUtils/serivce/MissionService.java
@@ -1,7 +1,29 @@
 package tzy.qrecitewords.dataUtils.serivce;
 
+import com.raizlabs.android.dbflow.sql.language.Select;
+
+import java.sql.Date;
+
+import tzy.qrecitewords.javabean.MissionOfDay;
+import tzy.qrecitewords.javabean.MissionOfDay_Table;
+
 /**
  * Created by tzy on 2016/5/9.
  */
 public class MissionService {
+    /**查询学习今日任务，如果没有则生成一个**/
+    public static MissionOfDay queryTodayMission(Date date){
+       MissionOfDay missionOfDay = new Select().from(MissionOfDay.class)
+                    .where(MissionOfDay_Table.date.eq(date))
+                    .querySingle();
+        if(missionOfDay == null){//今日任务还没有生成，则生成一个
+            missionOfDay =new MissionOfDay();
+            missionOfDay.setCountOfLearned(0);
+            missionOfDay.setDate(date);
+            missionOfDay.setTodayWords(60);
+            missionOfDay.insert();
+            return missionOfDay;
+        }
+        return missionOfDay;
+    }
 }
--- app/src/main/java/tzy/qrecitewords/javabean/MissionOfDay.java
+++ app/src/main/java/tzy/qrecitewords/javabean/MissionOfDay.java
@@ -1,7 +1,65 @@
 package tzy.qrecitewords.javabean;
 
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.NotNull;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.annotation.Unique;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import java.sql.Date;
+
+import tzy.qrecitewords.dataUtils.dbutils.WordDataBase;
+
 /**
  * Created by tzy on 2016/5/9.
  */
-public class MissionOfDay {
+@Table(database = WordDataBase.class)
+public class MissionOfDay extends BaseModel {
+
+    @PrimaryKey(autoincrement = true)
+    Long id;
+
+    @Column
+    @Unique
+    @NotNull
+    Date date;
+
+    @Column
+    int todayWords;//今日要学的单词的数量
+
+    @Column
+    int countOfLearned;//今日已学的单词的数量
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getTodayWords() {
+        return todayWords;
+    }
+
+    public void setTodayWords(int todayWords) {
+        this.todayWords = todayWords;
+    }
+
+    public int getCountOfLearned() {
+        return countOfLearned;
+    }
+
+    public void setCountOfLearned(int countOfLearned) {
+        this.countOfLearned = countOfLearned;
+    }
 }
